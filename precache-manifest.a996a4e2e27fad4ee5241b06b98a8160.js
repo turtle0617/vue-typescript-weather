6b7a1e379d4b4f1a23c26f76b3929dc683b09bfd
張睿峰 <qqaz50313@gmail.com>
2019-09-03 12:00:59 +0800

    deploy

---
+++ precache-manifest.a996a4e2e27fad4ee5241b06b98a8160.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-typescript-weather/robots.txt"
+  },
+  {
+    "revision": "d8e678f7226fa7530b63",
+    "url": "/vue-typescript-weather/js/chunk-vendors.734c33df.js"
+  },
+  {
+    "revision": "4f3b39b16b491afe9c24",
+    "url": "/vue-typescript-weather/js/app.048ba9b0.js"
+  },
+  {
+    "revision": "8071f9b1af793549ba4c643653b2470b",
+    "url": "/vue-typescript-weather/index.html"
+  },
+  {
+    "revision": "4f3b39b16b491afe9c24",
+    "url": "/vue-typescript-weather/css/app.2c54cfca.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/vue-typescript-weather/precache-manifest.a996a4e2e27fad4ee5241b06b98a8160.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vue-typescript-weather"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
